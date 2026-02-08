1f49185bb4f359865fa0226bafe1e712eab8b720
Vinzette <apoorva.kr03@gmail.com>
2026-02-08 12:03:17 +0000

    Implement Chaining

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough, RunnableLambda
+from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 
 load_dotenv()
-
+parser = StrOutputParser()
 #1. INDEXING
 #DOC INGGESTION CODEEEE
 video_id = "Gfr50f6ZBvo" # only the ID, not full URL
@@ -18,19 +20,13 @@ try:
     transcript = " ".join(
         snippet.text for snippet in fetched_transcript
     )
-
-    # Flatten it to plain text
-    
-    print(transcript)
-
 except TranscriptsDisabled:
     print("No captions available for this video.")
 
 #TEXT SPLIT
 splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 chunks = splitter.create_documents([transcript])
-len(chunks)
-chunks[100]
+
 
 #EMBEDDING GENERATION AND STORING IN VEC STORE
 embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
@@ -40,7 +36,7 @@ vector_store.get_by_ids(['2436bdb8-3f5f-49c6-8915-0c654c888700'])
 
 #2. Retrieval
 retriever = vector_store.as_retriever(search_type="similarity", search_kwargs={"k": 4})
-print(retriever.invoke('What is deepmind'))
+# print(retriever.invoke('What is deepmind'))
 
 #3. Augmentation
 model=ChatOpenAI()
@@ -56,15 +52,33 @@ prompt = PromptTemplate(
     input_variables = ['context', 'question']
 )
 question          = "is the topic of nuclear fusion discussed in this video? if yes then what was discussed"
-retrieved_docs    = retriever.invoke(question)
+#retrieved_docs    = retriever.invoke(question)
 
-context_text = "\n\n".join(doc.page_content for doc in retrieved_docs)
+#context_text = "\n\n".join(doc.page_content for doc in retrieved_docs)
 
-final_prompt = prompt.invoke({"context": context_text, "question": question})
+#final_prompt = prompt.invoke({"context": context_text, "question": question})
 
 #Generation
-answer = model.invoke(final_prompt)
-print(answer.content)
+#answer = model.invoke(final_prompt)
+#print(answer.content)
+
+#CHAINING
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+parallel_chain = RunnableParallel({
+    'context': retriever | RunnableLambda(format_docs),
+    'question': RunnablePassthrough()
+})
+
+parallel_chain.invoke('who is Demis')
+
+main_chain = parallel_chain | prompt | model | parser
+
+print(main_chain.invoke('Can you tell me what they discussed about quantum mechanics?'))
+
+
+
 
 
 
